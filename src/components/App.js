9ce67844c20aef5ddddbe490db17b2d8bb72052d
Blanca <blancabcbt@gmail.com>
2022-08-26 17:42:04 +0200

    add fecth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-
+import CallToApi from '../services/CallToApi';
+import { useState, useEffect } from 'react';
 import '../styles/App.scss';
 
 function App() {
+
+  //useEffect
+  useEffect(()=>{
+    CallToApi().then(dataApi=>{
+      console.log(dataApi);
+    })
+  },[]);
   return (
     <h1>Hola mundo</h1>
   );
 }
 
 export default App;
+
---
+++ src/services/CallToApi.js
@@ -0,0 +1,24 @@
+function CallToApi(){
+    return fetch('https://hp-api.herokuapp.com/api/characters').then(response=>response.json())
+    .then(data=>{
+        const cleanedData = data.map(character=>{
+            return{
+                name: character.name,
+                image: character.image,
+                alive: character.alive? "viv@" : "muert@",
+                species:character.species,
+                gender:character.gender,
+                house: character.house,
+                alt_name:character.alternate_name,
+
+            }
+        });
+        
+        return cleanedData;
+    });
+}
+
+export default CallToApi;
+
+
+//{"name":"Harry Potter","alternate_names":[],"species":"human","gender":"male","house":"Gryffindor","dateOfBirth":"31-07-1980","yearOfBirth":1980,"wizard":true,"ancestry":"half-blood","eyeColour":"green","hairColour":"black","wand":{"wood":"holly","core":"phoenix feather","length":11},"patronus":"stag","hogwartsStudent":true,"hogwartsStaff":false,"actor":"Daniel Radcliffe","alternate_actors":[],"alive":true,"image":"http://hp-api.herokuapp.com/images/harry.jpg"}
