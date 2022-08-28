96eb92fd24173262da471e2553fe6c17f7fc7ccd
Blanca <blancabcbt@gmail.com>
2022-08-28 14:05:39 +0200

    add styles to detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,3 +1,6 @@
+import '../styles/layout/CharacterDetail.scss';
+
+
 function CharacterDetail (props){
     const getAltName=()=>{
         if(props.character.altname.length !== 0){
@@ -7,14 +10,15 @@ function CharacterDetail (props){
 
 
     return(
-        <section>
+        <section className='detail'>
             <img 
+            className='detail__img'
             src={props.character.image}
             alt={`Foto de ${props.character.name}`}
             title={`Foto de ${props.character.name}`}/>
-            <section>
-                <h2>Nombre:{props.character.name}</h2>
-                <p>
+            <section className='detail__section'>
+                <h2 className='detail__section--name'>Nombre:{props.character.name}</h2>
+                <p className='detail__section--species'>
                     Especie: {props.character.species}<br/>
                     Estatus: {props.character.alive}<br/>
                     Género: {props.character.gender}<br/>
